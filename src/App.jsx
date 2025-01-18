43ee634623f77b27061fd6aec81f14acf7a87227
Jose Carmona <josshue85@gmail.com>
2025-01-17 23:48:20 -0300

    Iniciando desarrollo del componente <Pizza />

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import './App.css'
 // import Cart from './components/Cart'
 import Footer from './components/Footer'
-import Home from './components/Home'
+// import Home from './components/Home'
+// import RegisterPage from './components/RegisterPage'
 // import LoginPage from './components/LoginPage'
 import Navbar from './components/Navbar'
-// import RegisterPage from './components/RegisterPage'
+import Pizza from './components/Pizza'
 
 function App() {
   return (
     <div className="layout">
       {/* SOLO SE MOSTRARÁN LOS COMPONENTES NAVBAR, HOME Y FOOTER */}
       <Navbar />
-      <Home />
+      {/* <Home /> */}
       {/* <RegisterPage /> */}
       {/* <LoginPage /> */}
       {/* <Cart /> */}
+      <Pizza />
       <Footer />
     </div>
   )
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,8 @@
+const Pizza = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Pizza
